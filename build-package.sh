b325543e898d595ac6d141954744b0cbd4b90ffd
Phobos <josegithubacc@gmail.com>
2024-11-05 07:27:17 -0300

    Update build-package.sh

--- build-package.sh
+++ build-package.sh
@@ -481,7 +481,7 @@ while (($# >= 1)); do
 		-i)
 			if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
 				termux_error_exit "./build-package.sh: option '-i' is not available for on-device builds"
-			elif [ "$TERMUX_PREFIX" != "/data/data/com.termux/files/usr" ]; then
+			elif [ "$TERMUX_PREFIX" != "/data/data/com.itsaky.androidide/files/usr" ]; then
 				termux_error_exit "./build-package.sh: option '-i' is available only when TERMUX_APP_PACKAGE is 'com.termux'"
 			else
 				export TERMUX_INSTALL_DEPS=true
